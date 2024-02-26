1aa8046eb477f7e392eebca86a4c643e34b5b214
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2024-02-26 17:44:17 +0000

    gitapp

---
+++ app/src/main/java/aplication/App.java
@@ -0,0 +1,11 @@
+package applcation;
+
+import org.springframework.boot.autoconfigure.SpringBootApplcation;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplcation
+public class App{
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);       
+    }
+}
